f8e0bd5c8b67b700602e411d7dc8c3a9499a9d23
Emina Vejsilović <132358703+Emina89@users.noreply.github.com>
2024-01-26 23:01:52 +0100

    UcenjeCS

---
+++ CSHARP/UcenjeWP3/UcenjeCS/E01UlazIzlaz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E01UlazIzlaz
+    {
+
+public static void Izvedi()
+        {
+
+            string Ime;
+            Console.Write("Unesi Ime :");
+
+            Ime = Console.ReadLine();
+
+            Console.WriteLine(Ime);
+
+        }
+
+
+
+
+
+
+
+    }
+}
